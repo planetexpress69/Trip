b45632a9357dbb95b773d1c9ac88205be8a76ea6
Martin Kautz <kautz@ocean-insights.com>
2016-10-12 23:00:53 +0200

    Initial Commit

---
+++ Trip/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Trip
+//
+//  Created by Martin Kautz on 12.10.16.
+//  Copyright © 2016 Raketenmann. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
