f34915e558f4e9da08cd298f2ef81fd9ee1e2ce3
Martin Kautz <kautz@ocean-insights.com>
2016-10-13 22:49:19 +0200

    Added speed calculation, mocked UI

--- Trip/ViewController.swift
+++ Trip/ViewController.swift
@@ -10,16 +10,32 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var numberOfTrackpointsField: NSTextField?
+    @IBOutlet weak var distanceField: NSTextField?
+    @IBOutlet weak var maxSpeedField: NSTextField?
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        NotificationCenter.default.addObserver(self, selector: #selector(self.showData), name: NSNotification.Name(rawValue: "DidGetData"), object:nil)
     }
 
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
         }
+    }
+
+    func showData(notification: NSNotification) -> () {
+        let dict = notification.object as! NSDictionary
+        let numOfPos = dict["numOfPos"]
+        let completeDistance = dict["completeDistance"]
+        let maxSpeed = dict["maxSpeed"]
+        numberOfTrackpointsField?.stringValue = "\(numOfPos as! String)"
+        distanceField?.stringValue = "\(completeDistance as! String) nm"
+        maxSpeedField?.stringValue = "\(maxSpeed as! String) knots"
     }
 
 }
--- Trip/WindowController.swift
+++ Trip/WindowController.swift
@@ -52,29 +52,68 @@ class WindowController: NSWindowController {
                 let xmlDoc = try AEXMLDocument(xml: data)
                 var prevLat:Double = 0.0
                 var prevLon:Double = 0.0
+                var prevTime: Date = Date()
                 var completeDistance:Double = 0.0
+                var maxSpeed: Double = 0.0
+                var timeOfMaxSpeed: Date = Date()
+                var prevTimeOfMaxSpeed: Date = Date()
+                let infinity = Double.infinity
                 var i = 0
+                var tick = 1;
                 for item in xmlDoc.root["trk"].children {
                     if item.name == "trkseg" {
                         for pos in item.children {
-                            if let lat = pos.attributes["lat"]  {
-                                if let lon = pos.attributes["lon"]  {
-                                    if prevLat != 0.0 {
-                                        completeDistance += (self.haversineDinstance(la1: prevLat, lo1: prevLon, la2: lat.doubleValue, lo2: lon.doubleValue))
+
+                            tick = tick + 1
+
+                            if (tick % 15 == 0) {
+
+
+                                if let lat = pos.attributes["lat"]  {
+                                    if let lon = pos.attributes["lon"]  {
+                                        if let time = pos.children[0].value {
+                                            if let datetime = self.stringToDate(sDate: time) {
+
+                                                if prevLat != 0.0 {
+                                                    let distance = (self.haversineDinstance(la1: prevLat, lo1: prevLon, la2: lat.doubleValue, lo2: lon.doubleValue))
+                                                    completeDistance += distance
+                                                    let diff = (datetime.timeIntervalSince(prevTime))
+                                                    let speed = (distance / diff * 3.6 / 1.852)
+
+                                                    if speed < infinity && speed > maxSpeed {
+                                                        timeOfMaxSpeed = datetime
+                                                        prevTimeOfMaxSpeed = prevTime
+                                                        maxSpeed = speed
+                                                    }
+
+                                                }
+                                                prevLat = lat.doubleValue
+                                                prevLon = lon.doubleValue
+                                                prevTime = datetime
+                                            }
+                                        }
+
                                     }
-                                    prevLat = lat.doubleValue
-                                    prevLon = lon.doubleValue
                                 }
+                                i = i + 1;
                             }
-                            i = i + 1;
+
                         }
                     }
                 }
 
                 DispatchQueue.main.async {
                     print("Number of pos: \(i)")
-                    print("Distance     : \(completeDistance / 1000) km")
-                    NotificationCenter.default.post(name: Notification.Name(rawValue: "DidGetData"), object: nil, userInfo: nil);
+                    print("Distance     : \(completeDistance / 1000 / 1.852) nm")
+                    print("Max speed    : \(maxSpeed) knots @ \(prevTimeOfMaxSpeed) - \(timeOfMaxSpeed)")
+
+                    let result: [String:String] = [
+                        "numOfPos": String(i),
+                        "completeDistance": String((completeDistance / 1000 / 1.852).roundTo(places: 3)),
+                        "maxSpeed": String(maxSpeed.roundTo(places: 3))
+                    ]
+
+                    NotificationCenter.default.post(name: Notification.Name(rawValue: "DidGetData"), object: result, userInfo: nil);
                 }
             }
             catch {
@@ -82,6 +121,12 @@ class WindowController: NSWindowController {
             }
 
         }
+    }
+
+    func stringToDate(sDate:String) -> Date? {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'"
+        return formatter.date(from: sDate)
     }
 
     func haversineDinstance(la1: Double, lo1: Double, la2: Double, lo2: Double, radius: Double = 6367444.7) -> Double {
@@ -93,17 +138,17 @@ class WindowController: NSWindowController {
         let ahaversin = { (angle: Double) -> Double in
             return 2*asin(sqrt(angle))
         }
-
+        
         // Converts from degrees to radians
         let dToR = { (angle: Double) -> Double in
             return (angle / 360) * 2 * M_PI
         }
-
+        
         let lat1 = dToR(la1)
         let lon1 = dToR(lo1)
         let lat2 = dToR(la2)
         let lon2 = dToR(lo2)
-
+        
         return radius * ahaversin(haversin(lat2 - lat1) + cos(lat1) * cos(lat2) * haversin(lon2 - lon1))
     }
 }
@@ -111,5 +156,12 @@ class WindowController: NSWindowController {
 extension String {
     var doubleValue: Double {
         return (self as NSString).doubleValue
+    }
+}
+
+extension Double {
+    func roundTo(places:Int) -> Double {
+        let divisor = pow(10.0, Double(places))
+        return (self * divisor).rounded() / divisor
     }
 }
